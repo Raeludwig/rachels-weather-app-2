601a9469c6f03f547120a66e51608e0cf0d33029
rachel schumann <racheleludwig@gmail.com>
2023-01-30 22:45:21 -0700

    cities and temp are working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,76 @@
 var APIKey = "f4cc82078e0999601d888c5956e645ef";
-var city;
+var city = document.querySelector("#city-selector");
 var submitBtn = document.querySelector('button');
+var fiveDayEl = document.querySelector('.five-day')
 
-
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-fetch(queryURL);
+// fetch(queryURL);
 
 // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`;
 
 // TODO: is the following link what I need to put in for the option to search for more than just a city?
 https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
 
-// submitBtn.addEventListener('click', function (){
-//     fetch(queryURL)
-// .then(response => Response.json())
-// .then(data => console.log(data))
+function weatherSearch() {
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city.value + "&appid=" + APIKey;
 
-// .catch (err => alert ("wrong city name"))
+    fetch(queryURL)
+        .then(response => response.json())
+        .then(data => {
+            console.log(city.value)
+            renderFiveDay(data)
+        })
+    saveHistory()
+
+}
+
+function saveHistory() {
+    var cityHistory = JSON.parse(localStorage.getItem('history')) || []
+    cityHistory.push(city.value)
+    console.log(cityHistory)
+
+
+    localStorage.setItem('history', JSON.stringify(cityHistory))
+
+}
+
+
+
+function renderFiveDay(weather) {
+    console.log(weather)
+    for (var i = 0; i < weather.list.length; i = i + 8) {
+        console.log(weather.list[i])
+        var card = document.createElement('div')
+        var cityTemp = document.createElement('h3')
+        card.setAttribute("class", "card")
+        cityTemp.textContent = `temp: ${weather.list[i].main.temp}`
+        fiveDayEl.appendChild(card)
+        card.append(cityTemp)
+
+    }
+
+}
+
+
+
+
+
+
+
+
+submitBtn.addEventListener('click', weatherSearch);
+
+
+
+
+
+
+
+// fetch(queryURL)
+// .then(function(response){
+//     return response.json()
 // })
-
-fetch(queryURL)
-.then(function(response){
-    return response.json()
-})
-.then(function(data){
-    console.log(data)
-})
+// .then(function(data){
+//     console.log(data)
+//     console.log(city)
+// })
 //making a change
