7ac8866b58ba1726f69b1a2c87040745b795bceb
Raeludwig <racheleludwig@gmail.com>
2023-01-31 08:58:30 -0700

    wind speed, temp, and humidity showing up

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,26 +34,67 @@ function saveHistory() {
 
 }
 
-
+//converts temp
+function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("temp").innerHTML=((valNum-273.15)*1.8)+32;
+  }
 
 function renderFiveDay(weather) {
     console.log(weather)
     for (var i = 0; i < weather.list.length; i = i + 8) {
         console.log(weather.list[i])
         var card = document.createElement('div')
+        //shows the temp
         var cityTemp = document.createElement('h3')
-        card.setAttribute("class", "card")
-        cityTemp.textContent = `temp: ${weather.list[i].main.temp}`
-// var windSpeed=document.createElement('h3')
-// windSpeed.textContent = 'windSpeed: ${weather.list[i].wind.speed}'
-var cityHumidity = document.querySelector('#humidity')
-$("#humidity").text(today.humidity);
+         cityTemp.textContent = `temp: ${weather.list[i].main.temp}`
+         //shows the windspeed
+var cityWindSpeed = document.createElement('h3')
+cityWindSpeed.textContent= `wind speed: ${weather.list[i].wind.speed}`
+//shows the humidity
+var cityHumidity = document.createElement('h3')
+cityHumidity.textContent= `humidity: ${weather.list[i].main.humidity}%`
+//shows condition
+// var cityCondition = document.createElement('h3')
+// cityCondition.textContent= `conditions: ${weather.list[i].weather.[0].main}`
+//shows icon
 
 
 
+
+card.setAttribute("class", "card")
         fiveDayEl.appendChild(card)
         card.append(cityTemp)
+        card.append(cityHumidity)
+        card.append(cityWindSpeed)
+        // card.append(cityCondition)
 
+
+
+
+
+
+        // .then(function (response) {
+        //     console.log(response)
+        //     //to avoid repeating city information on button click 
+        //     $(".cityList").empty()
+        //     $("#days").empty()
+           
+        //     var cityMain1 = $("<div col-12>").append($("<p><h2>" + response.name + ' (' + currentDate + ')' + "</h2><p>"));
+        //     var image = $('<img class="imgsize">').attr('src', 'http://openweathermap.org/img/w/' + response.weather[0].icon + '.png');        
+        //     var degreeMain = $('<p>').text('Temperature : ' + response.main.temp + ' °F ');
+        //     var humidityMain = $('<p>').text('Humidity : ' + response.main.humidity + '%');
+        //     var windMain = $('<p>').text('Wind Speed : ' + response.wind.speed + 'MPH');       
+        //     var uvIndexcoord = '&lat=' + response.coord.lat + '&lon=' + response.coord.lon;
+        //     var cityId = response.id;
+    
+        //     displayUVindex(uvIndexcoord);
+        //     displayForecast(cityId);
+    
+        //     cityMain1.append(image).append(degreeMain).append(humidityMain).append(windMain);
+        //     $('#cityList').empty();
+        //     $('#cityList').append(cityMain1);
+        // });
     }
 
 }
